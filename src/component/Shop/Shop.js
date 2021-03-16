20e2166db42dac962112bdc2233348eb03a59e21
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-03-16 17:52:13 +0600

    ema john with ph

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import fakeData from "../../fakeData";
+import { addToDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handlerAddProduct = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+        const sameProduct = newCart.filter(pd => pd.key === product.key)
+        const count = sameProduct.length
+        addToDatabaseCart(product.key, count)
+    }
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    products.map(pd => <Product key={pd.key} product={pd} showAddToCart={true} handlerAddProduct={handlerAddProduct}></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
